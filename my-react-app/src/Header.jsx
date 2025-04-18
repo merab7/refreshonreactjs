cf6e9f2cf0862063904eb0069bfa571b2794e08b
merab7 <merabtodua7@gmail.com>
2025-04-18 18:38:18 +0400

    initial commit

---
+++ my-react-app/src/Header.jsx
@@ -0,0 +1,29 @@
+function Header() {
+  return (
+    <header>
+      <h1>My website</h1>
+      <nav>
+        <ul>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Services</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+          <li>
+            <a href="#">this is form zed</a>
+          </li>
+        </ul>
+        <hr />
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
